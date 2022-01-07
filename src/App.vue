88188e6803c92269e0a697f60148b8ac3bd020aa
Bogdan <zbohdan7@gmail.com>
2022-01-07 15:10:29 +0200

    Added Bootstrap-Vue. Changed menu to bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <sui-menu pointing top fixed>
+    <!-- <sui-menu pointing top fixed>
       <sui-menu-item>
         <p>VueJS App</p>
       </sui-menu-item>
@@ -10,7 +10,25 @@
       <sui-menu-item>
         <router-link to="/create">Create</router-link>
       </sui-menu-item>
-    </sui-menu>
+    </sui-menu> -->
+    <b-navbar toggleable="lg" type="dark" variant="danger">
+      <b-navbar-brand href="#">
+        <img alt="Vue logo" src="./assets/logo.png" style="width: 60px; padding-left: 1em" />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="navbar"></b-navbar-toggle>
+
+      <b-collapse id="navbar" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
+          </b-nav-item>
+          <b-nav-item>
+            <b-nav-item :to="{name: 'create'}">Create</b-nav-item>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <img alt="Vue logo" src="./assets/logo.png" />
     <router-view></router-view>
   </div>
@@ -29,6 +47,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 40px;
+  margin-top: 0.5em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
+import {BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { routes } from './routes'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(SuiVue)
 Vue.use(VueRouter)
 
